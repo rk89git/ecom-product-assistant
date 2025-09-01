fc4767454b15103c0bcb8e673dc9150c3b9a7d27
Rakesh Kumar <v-rakekuma@microsoft.com>
2025-09-01 10:41:26 +0530

    check package versions

---
+++ get_lib_versions.py
@@ -0,0 +1,14 @@
+import importlib.metadata
+packages = [
+    "langchain",
+    "python-dotenv",
+    "langchain_core"
+]
+for pkg in packages:
+    try:
+        version = importlib.metadata.version(pkg)
+        print(f"{pkg}: {version}")
+    except importlib.metadata.PackageNotFoundError:        
+        print(f"{pkg} (not installed)")
+
+
