e34f8379d63aa50a68ef4f418c278ec5074382f4
rk89git <v-rakekuma@microsoft.com>
2025-09-05 22:56:25 +0530

    Updated Code

---
+++ prod_assistant/logger/__init__.py
@@ -0,0 +1,14 @@
+import logging
+import sys
+
+# Configure logging
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    handlers=[
+        logging.StreamHandler(sys.stdout)
+    ]
+)
+
+# Create the global logger
+GLOBAL_LOGGER = logging.getLogger('prod_assistant')
